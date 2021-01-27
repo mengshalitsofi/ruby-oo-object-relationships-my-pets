90d2283b31e9756924e9ff2f4bc6696090d88b42
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-27 14:20:13 -0800

    commit tree 509692d87b0c47bbf0bcb483a5ff0489bc34b329

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_accessor :owner
+  attr_reader :name
+  attr_accessor :mood
+  @@all = []
+
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
